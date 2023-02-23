50d2aed68ef8b9394cc09087aa12d1c7e4cc4f4e
Lokteeva <swsw06@mail.ru>
2023-02-23 11:53:32 +0300

    day2

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose'
 import {registerValidation} from './validation/auth.js'
 import {validationResult} from 'express-validator'
-import UserModel from './models/User.js'
 import bcrypt from 'bcrypt'
+import UserModel from './models/User.js'
+import checkAuth from './utils/checkAuth.js'
+
 
 mongoose
     .connect(`mongodb+srv://admin:admin@cluster0.nywzjzo.mongodb.net/blog?retryWrites=true&w=majority`)
@@ -15,6 +17,50 @@ const app = express()
 
 app.use(express.json())
 
+app.post('/auth/login', async (req, res) => {
+  try {
+    const user = await UserModel.findOne({email: req.body.email})
+
+    if (!user) {
+        return res.status(404).json({
+            massage: 'Не удалось найти пользователя'
+        })
+    }
+
+    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+
+    if(!isValidPass)   
+    if (!user) {
+      return res.status(404).json({
+          massage: 'Не верный логин или пароль'
+
+      })
+  }
+
+  const token = jwt.sign(
+  {
+    _id: user._id
+  },
+  'secret123',
+  {
+    expiresIn: '30d'
+  },
+)
+
+const {passwordHash, ...userData} = user.doc;
+
+    res.json({
+      ...userData,
+      token,
+    })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({
+        massage: 'Не удалось авторизоваться'
+    })
+  }
+})
+
 app.post('/auth/register', registerValidation, async (req, res) => {
   try {
     const errors = validationResult(req)
@@ -24,24 +70,47 @@ app.post('/auth/register', registerValidation, async (req, res) => {
 
     const password = req.body.password;
     const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
+    const hash = await bcrypt.hash(password, salt);
     
     const doc = new UserModel({
         email: req.body.email,
         fullName: req.body.fullName,
         avatarURL: req.body.avatarURL,
-        passwordHash
+        passwordHash: hash
     })
     const user = await doc.save()
 
-    res.json(user)
-  } catch (error) {
+    const token = jwt.sign(
+    {
+      _id: user._id
+    },
+    'secret123',
+    {
+      expiresIn: '30d'
+    }
+)
+
+    const {passwordHash, ...userData} = user.doc;
+
+    res.json({
+      ...userData,
+      token,
+    })
+    } catch (error) {
+    console.log(err)
     res.status(500).json({
         massage: 'Не удалось зарегистрироваться'
     })
   }  
 })
 
+app.get('/auth/me', checkAuth, (req, res) => {
+  try {
+    res.json({
+      success: true,
+    })
+  } catch (error) {}
+})
   
 app.listen(4444, (err) => {
     if (err) {
---
+++ utils/checkAuth.js
@@ -0,0 +1,15 @@
+import Jwt  from "jsonwebtoken";
+
+export default (req, res, next) => {
+const token = req.headers.authorization;
+
+if(token) {
+    try {
+        const decoded = jwt.verify(token, 'secret123')
+    } catch (e) {}
+} else {
+    return res.status(403).json({
+        message: "Нет доступа"
+    })
+}
+}
